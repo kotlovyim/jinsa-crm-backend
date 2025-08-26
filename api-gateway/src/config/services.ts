57509e8034f04c680904702934557a4515092789
kotlovyim <kotlovyim@gmail.com>
2025-08-26 10:54:36 +0300

    feat(config): added other services to gatawey config

--- api-gateway/src/config/services.ts
+++ api-gateway/src/config/services.ts
@@ -4,4 +4,8 @@ export const serviceRegistry: ServiceRegistry = {
     iam: process.env["IAM_SERVICE_URL"] || "http://localhost:3001",
     projects: process.env["PROJECT_SERVICE_URL"] || "http://localhost:3002",
     hr: process.env["HR_SERVICE_URL"] || "http://localhost:3003",
+    calendar: process.env["CALENDAR_SERVICE_URL"] || "http://localhost:3004",
+    chat: process.env["CHAT_SERVICE_URL"] || "http://localhost:3005",
+    notification:
+        process.env["NOTIFICATION_SERVICE_URL"] || "http://localhost:3006",
 };
